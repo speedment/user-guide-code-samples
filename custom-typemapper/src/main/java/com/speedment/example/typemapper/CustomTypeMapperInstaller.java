cd4880de8d6b589e321f61ab560126d6f44a916c
Per-Åke Minborg <minborg@speedment.com>
2019-12-05 16:48:02 +0100

    Remove dependency on generator-translator

--- custom-typemapper/src/main/java/com/speedment/example/typemapper/CustomTypeMapperInstaller.java
+++ custom-typemapper/src/main/java/com/speedment/example/typemapper/CustomTypeMapperInstaller.java
@@ -20,8 +20,7 @@ import static com.speedment.common.injector.State.INITIALIZED;
 import static com.speedment.common.injector.State.RESOLVED;
 import com.speedment.common.injector.annotation.ExecuteBefore;
 import com.speedment.common.injector.annotation.WithState;
-import com.speedment.generator.translator.component.TypeMapperComponent;
-
+import com.speedment.runtime.typemapper.TypeMapperComponent;
 /**
  *
  * @author Per Minborg
--- custom-typemapper/src/main/java/module-info.java
+++ custom-typemapper/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module custom.typemapper {
     exports com.speedment.example.typemapper;
 
     requires transitive com.speedment.common.injector;
-    requires transitive com.speedment.generator.translator;
     requires transitive com.speedment.runtime.config;
     requires transitive com.speedment.runtime.typemapper;
 }
